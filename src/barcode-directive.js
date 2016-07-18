070d70c82c58bd6b124a20f42085e62da9a3cc29
Paul Biester <pabiester@airfrance.fr>
2016-07-18 14:01:46 +0200

    Bugfix

--- src/barcode-directive.js
+++ src/barcode-directive.js
@@ -21,28 +21,6 @@ export default class BarcodeDirective {
 
     constructor() {
 
-        this.defaults = {
-            format: 'CODE128',
-            lineColor: '#000000',
-            width: 2,
-            height: 100,
-            displayValue: true,
-            fontOptions: '',
-            font: 'monospace',
-            textAlign: 'center',
-            textPosition: 'bottom',
-            textMargin: 2,
-            fontSize: 20,
-            background: '#ffffff',
-            margin: 10,
-            marginTop: undefined,
-            marginBottom: undefined,
-            marginLeft: undefined,
-            marginRight: undefined,
-            valid: function(valid){}
-        };
-
-
         // Directive config
         this.replace = true;
         this.restrict = 'E';
@@ -75,27 +53,48 @@ export default class BarcodeDirective {
 
     link(scope, element, attributes) {
 
+		let defaults = {
+            format: 'CODE128',
+            lineColor: '#000000',
+            width: 2,
+            height: 100,
+            displayValue: true,
+            fontOptions: '',
+            font: 'monospace',
+            textAlign: 'center',
+            textPosition: 'bottom',
+            textMargin: 2,
+            fontSize: 20,
+            background: '#ffffff',
+            margin: 10,
+            marginTop: undefined,
+            marginBottom: undefined,
+            marginLeft: undefined,
+            marginRight: undefined,
+            valid: function(valid){}
+        };
+	
         scope.$watch(() => {
             if(scope.bcText !== undefined && scope.bcText.length > 0) {
                 JsBarcode(element[0], scope.bcText, {
-                    format: scope.bcFormat || this.defaults.format,
-                    lineColor: scope.bcLineColor || this.defaults.lineColor,
-                    width: parseInt(scope.bcWidth, 10) || this.defaults.width,
-                    height: parseInt(scope.bcHeight, 10) || this.defaults.height,
-                    displayValue: (scope.bcDisplayValue === 'true') || this.defaults.displayValue,
-                    fontOptions: scope.bcFontOptions || this.defaults.fontOptions,
-                    font: scope.bcFont || this.defaults.font,
-                    textAlign: scope.bcTextAlign || this.defaults.textAlign,
-                    textPosition: scope.bcTextPosition || this.defaults.textPosition,
-                    textMargin: parseInt(scope.bcTextMargin, 10) || this.defaults.textMargin,
-                    fontSize: parseInt(scope.bcFontSize, 10) || this.defaults.fontSize,
-                    background: scope.bcBackground || this.defaults.background,
-                    margin: parseInt(scope.bcMargin, 10) || this.defaults.margin,
-                    marginTop: parseInt(scope.bcMarginTop, 10) || this.defaults.marginTop,
-                    marginBottom: parseInt(scope.bcMarginBottom, 10) || this.defaults.marginBottom,
-                    marginLeft: parseInt(scope.bcMarginLeft, 10) || this.defaults.marginLeft,
-                    marginRight: parseInt(scope.bcMarginRight, 10) || this.defaults.marginRight,
-                    valid: scope.bcValid || this.defaults.valid
+                    format: scope.bcFormat || defaults.format,
+                    lineColor: scope.bcLineColor || defaults.lineColor,
+                    width: parseInt(scope.bcWidth, 10) || defaults.width,
+                    height: parseInt(scope.bcHeight, 10) || defaults.height,
+                    displayValue: (scope.bcDisplayValue === 'true') || defaults.displayValue,
+                    fontOptions: scope.bcFontOptions || defaults.fontOptions,
+                    font: scope.bcFont || defaults.font,
+                    textAlign: scope.bcTextAlign || defaults.textAlign,
+                    textPosition: scope.bcTextPosition || defaults.textPosition,
+                    textMargin: parseInt(scope.bcTextMargin, 10) || defaults.textMargin,
+                    fontSize: parseInt(scope.bcFontSize, 10) || defaults.fontSize,
+                    background: scope.bcBackground || defaults.background,
+                    margin: parseInt(scope.bcMargin, 10) || defaults.margin,
+                    marginTop: parseInt(scope.bcMarginTop, 10) || defaults.marginTop,
+                    marginBottom: parseInt(scope.bcMarginBottom, 10) || defaults.marginBottom,
+                    marginLeft: parseInt(scope.bcMarginLeft, 10) || defaults.marginLeft,
+                    marginRight: parseInt(scope.bcMarginRight, 10) || defaults.marginRight,
+                    valid: scope.bcValid || defaults.valid
                 })
             }
 
